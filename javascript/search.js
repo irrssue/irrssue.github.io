7bfe93233d48cddadd5578a02b8612065ad9a22e
Saw Thura Zaw <irrssue@gmail.com>
2025-11-12 19:51:13 -0500

    Revert "feat: make search result contexts clickable and scroll to exact match"
    This reverts commit bdeff6f92bef2161ae74f921e31c73592c124ac9.

--- javascript/search.js
+++ javascript/search.js
@@ -251,13 +251,10 @@
             `;
 
             results.writing.forEach(result => {
-                const urlWithSearch = `${result.url}&search=${encodeURIComponent(query)}`;
                 html += `
                     <div class="search-result-item">
-                        <a href="${urlWithSearch}" class="search-result-title">${highlightMatch(result.title, query)}</a>
-                        <a href="${urlWithSearch}" class="search-result-context-link">
-                            <p class="search-result-context">${highlightMatch(result.context, query)}</p>
-                        </a>
+                        <a href="${result.url}" class="search-result-title">${highlightMatch(result.title, query)}</a>
+                        <p class="search-result-context">${highlightMatch(result.context, query)}</p>
                         ${result.date ? `<span class="search-result-date">${new Date(result.date).getFullYear()}</span>` : ''}
                     </div>
                 `;
